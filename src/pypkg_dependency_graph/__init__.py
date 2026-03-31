c317bfe13d59e7b703061dd5f656e4c1a89dc5c5
Matthias Ossadnik <ossadnik.matthias@gmail.com>
2026-03-31 15:07:26 +0200

    add missing classes to public api

--- src/pypkg_dependency_graph/__init__.py
+++ src/pypkg_dependency_graph/__init__.py
@@ -1,5 +1,5 @@
 from .api import get_package_dependency_graph, get_package_tree
-from .package import Package, SubModule, SubPackage
+from .package import Package, SubModule, SubPackage, ResourceFile, LibraryModule
 
 
 __version__ = '0.1.0'
@@ -8,6 +8,8 @@ __all__ = [
     'Package',
     'SubModule',
     'SubPackage',
+    'ResourceFile',
+    'LibraryModule',
     'get_package_dependency_graph',
     'get_package_tree',
 ]
